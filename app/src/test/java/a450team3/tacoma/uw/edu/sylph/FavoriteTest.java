0ba885a49b5605e83912cd32c7692a9e02d7df1f
andyb <andybleich@gmail.com>
2016-12-07 21:58:04 -0800

    Added tests. They do not function.

--- app/src/test/java/a450team3/tacoma/uw/edu/sylph/FavoriteTest.java
+++ app/src/test/java/a450team3/tacoma/uw/edu/sylph/FavoriteTest.java
@@ -1,22 +1,54 @@
 package a450team3.tacoma.uw.edu.sylph;
 
+import junit.framework.Assert;
+
+import org.json.JSONArray;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import a450team3.tacoma.uw.edu.sylph.favorites.Favorite;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
 import static junit.framework.TestCase.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.when;
 
 /**
  * Testing class for Favorites Class.
  * Created by andyb on 12/6/2016.
  */
 
+@RunWith(org.mockito.junit.MockitoJUnitRunner.class)
 public class FavoriteTest {
 
+    @Mock
+    JSONArray mockJsonArray;
+
+    @Mock
+    Favorite mockFavorite;
+
+    /**Favorite for testing on */
     private Favorite testFav;
 
+    /**List for adding favorites to. */
+    private List<Favorite> favoriteList;
+
+    /**JSON string that should provide good results. */
+    private String goodJSONString = "{\"Account\":\"andybleich@gmail.com\",\"Title\":\"Ninjabread Man -\""
+            + "Game Grumps\",\"Description\":\"You cant catch me. Nor should you want to.\",\"URL\":" +
+            "\"https:\\/\\/www.youtube.com\\/watch?v=7gA_PU4VuhQ\"}";
+
+    /**
+     * Initializes favorites.
+     */
     @Before
     public void initFav() {
         testFav = new Favorite("andybleich@gmail.com", "Title", "Description",
@@ -24,12 +56,18 @@ public class FavoriteTest {
         //Url for Stunner dan le sac vs Scroobius Pip
     }
 
+    /**
+     * Tests Favorite constructor.
+     */
     @Test
     public void TestFavoriteConstructor() {
         assertNotNull(new Favorite("andybleich@gmail.com", "Title", "Description",
                 "http://www.youtube.com/watch?v=aFSD2IvXecs"));
     }
 
+    /**
+     * Tests all getters for Favorite.
+     */
     @Test
     public void TestGetters () {
         assertEquals("Title equals", "Title", testFav.getTitle());
@@ -37,5 +75,43 @@ public class FavoriteTest {
         assertEquals("Description Equals", "Description", testFav.getDescription());
         assertEquals("Url Equals", "http://www.youtube.com/watch?v=aFSD2IvXecs", testFav.getUrl());
     }
+
+
+    /**
+     * Tests parse method with bad json.
+     */
+    @Test
+    public void TestJsonBadJson (){
+        String string = Favorite.parseFavoriteJSON("thisIsABadStringForJSON", favoriteList);
+        System.out.print(string);
+        assertEquals(true, string.startsWith("Unable to"));
+    }
+
+    /**
+     * Tests parse method with good Json.
+     */
+    @Test
+    public void TestJsonGoodJson() {
+        when(mockJsonArray.length()).thenReturn(1);
+        String string = Favorite.parseFavoriteJSON(goodJSONString, favoriteList);
+        assertNull(string);
+    }
+
+    /**
+     * Tests equals method.
+     */
+    @Test
+    public void TestEquals() {
+        Favorite testFavEqual = new Favorite("andybleich@gmail.com", "Title", "Description",
+                "http://www.youtube.com/watch?v=aFSD2IvXecs");
+        Favorite testFavNotEqual = new Favorite("test@gmail.com", "Title", "Description",
+                "http://www.youtube.com/watch?v=aFSD2IvXecs");
+        assertTrue(testFav.equals(testFavEqual));
+        assertFalse(testFav.equals(testFavNotEqual));
+
+    }
+
+
+
 
 }
---
+++ app/src/test/java/a450team3/tacoma/uw/edu/sylph/FavoritesDBTest.java
@@ -0,0 +1,115 @@
+package a450team3.tacoma.uw.edu.sylph;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import a450team3.tacoma.uw.edu.sylph.data.FavoritesDB;
+import a450team3.tacoma.uw.edu.sylph.favorites.Favorite;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+/**
+ * Class to handle testing of FavroitesDB
+ * Created by andyb on 12/7/2016.
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class FavoritesDBTest {
+
+    @Mock
+    Context mockContext;
+
+    @Mock
+    FavoritesDB mFavDb;
+
+    @Mock
+    SQLiteDatabase mockSqlDB;
+
+    @Mock
+    FavoritesDB.FavoriteDBHelper mockSqlDBHelper;
+
+    /** String for accessing Account column. */
+    private String mDBAccountString = "Account";
+
+    /** String for accessing Title column. */
+    private String mDBTitleString = "Title";
+
+    /** String for accessing Description column. */
+    private String mDBDescString = "Description";
+
+    /** String for accessing URL column. */
+    private String mDBURLString = "URL";
+
+    /** Account signed in for testing. */
+    private String signedInAccount = "test@gmail.com";
+
+    /** Favorites used for testing */
+    private Favorite testFav1, testFav2;
+
+    /** List of Favorites */
+    private List<Favorite> mList;
+
+
+    @Before
+    public void before() {
+        when(mockContext.getString(R.string.fav_db_account)).thenReturn(mDBAccountString);
+        when(mockContext.getString(R.string.fav_db_title)).thenReturn(mDBTitleString);
+        when(mockContext.getString(R.string.fav_db_description)).thenReturn(mDBDescString);
+        when(mockContext.getString(R.string.fav_db_url)).thenReturn(mDBURLString);
+        when(mockContext.getString(R.string.create_FavDB_SQL)).thenReturn(
+                "CREATE TABLE IF NOT EXISTS Favorites" +
+                "    (Account VARCHAR(255), Title TEXT, Description TEXT, URL TEXT) ");
+        when(mockContext.getString(R.string.drop_FavDB_SQL))
+                .thenReturn("DROP TABLE IF EXISTS Favorites");
+        when(mockSqlDBHelper.getWritableDatabase()).thenReturn(mockSqlDB);
+        when(mFavDb.insertFavToDB(testFav1)).thenReturn(true);
+        when(mFavDb.getFavorites(signedInAccount)).thenReturn(mList);
+        mList = new ArrayList<>();
+        testFav1 = new Favorite(signedInAccount, "Stunner", "Pip",
+                "http://www.youtube.com/watch?v=aFSD2IvXecs");
+        testFav2 = new Favorite(signedInAccount, "Beat That my Heart Skipped", "Pip",
+                "https://www.youtube.com/watch?v=ESvYRR1Fyug");
+    }
+
+    /**
+     * Tests insert to DB method
+     */
+    @Test
+    public void insertFavToDBTest() {
+        assertEquals(false, mFavDb.insertFavToDB(testFav1));
+
+    }
+
+    /**
+     * Tests getFavorites method.
+     */
+    @Test
+    public void getFavoritesTest() {
+        mList.add(testFav1);
+        mList.add(testFav2);
+
+        List<Favorite> favoriteList = mFavDb.getFavorites(signedInAccount);
+
+        assertTrue(testFav1.equals(favoriteList.get(0)));
+        assertTrue(testFav2.equals(favoriteList.get(1)));
+    }
+
+
+
+
+
+
+}
